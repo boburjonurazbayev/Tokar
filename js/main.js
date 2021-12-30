becc9c26d9db497e2a9c696ce5dbe7cca7529f10
kvmvkxnt <kamron1007dada@gmail.com>
2021-12-30 16:54:10 +0500

    Added js to flags and more

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-const elNext = document.querySelector('.arrow--next')
-const elPrev = document.querySelector('.arrow--prev')
-const elImg = document.querySelector('.main__img')
-const elList = document.querySelector('.image__list1')
-const elButtons = document.querySelectorAll('.pop-button')
-const elPop = document.querySelector('.pop-ups')
-const elMain = document.querySelector('.main')
-const elsExit = document.querySelectorAll('.pop-exit')
-const elFlag = document.querySelectorAll('.flag__button')
-const elTelType = document.querySelectorAll('.tel-type')
-const elTelUkr = document.querySelectorAll('.button__ukr')
-const elTelRus = document.querySelectorAll('.button__rus')
-const elForm = document.querySelectorAll('.form__inputs')
-const elFlagArrow = document.querySelectorAll('.flag__arrow')
-const counter = 1;
-
-elNext.addEventListener('click', () => {
-    counter + 1
-    elList.classList.add(`image__list${counter}`)
-    console.log('+image')
-})
-
-elPrev.addEventListener('click', () => {
-    counter - 1;
-    elList.classList.add(`image__list${counter}`)
-    console.log('-image')
-})
+var elNext = document.querySelector('.arrow--next')
+var elPrev = document.querySelector('.arrow--prev')
+var elImg = document.querySelector('.main__img')
+var elList = document.querySelector('.image__list')
+var elButtons = document.querySelectorAll('.pop-button')
+var elPop = document.querySelector('.pop-ups')
+var elMain = document.querySelector('.main')
+var elsExit = document.querySelectorAll('.pop-exit')
+var elFlag = document.querySelectorAll('.flag__button')
+var elTelType = document.querySelectorAll('.tel-type')
+var elTelUkr = document.querySelectorAll('.button__ukr')
+var elTelRus = document.querySelectorAll('.button__rus')
+var elForm = document.querySelectorAll('.form__inputs')
+var elFlagArrow = document.querySelectorAll('.flag__arrow')
+let counter = 0;
 
 elButtons.forEach(element => {
     element.addEventListener('click', () => {
@@ -51,17 +39,48 @@ elFlag.forEach(element => {
 })
 
 elTelUkr.forEach(element => {
-    element.addEventListener('click', () => {
-        elForm.forEach(el => {
-            el.classList.remove('form__rus')
-        })
-    })
+    element.addEventListener('click', ukrFunction)
 })
 
 elTelRus.forEach(element => {
-    element.addEventListener('click', () => {
-        elForm.forEach(el => {
-            el.classList.add('form__rus')
-        })
-    })
+    element.addEventListener('click', rusFunction)
 })
+
+function rusFunction() {
+    elTelType.forEach(type => {
+        type.classList.remove('tel-type--active');
+    });
+    elForm.forEach(el => {
+        el.classList.add('form__rus')
+    });
+}
+
+function ukrFunction() {
+    elForm.forEach(el => {
+        el.classList.remove('form__rus')
+    });
+    elTelType.forEach(type => {
+        type.classList.remove('tel-type--active');
+    });
+}
+
+elNext.addEventListener('click', () => {
+    counter++;
+    elList.classList.add("image__list"+counter);
+    
+    if (counter > 3) {
+        counter = 0;
+        elList.classList.remove("image__list1", "image__list2", "image__list3");
+        elList.className = "image__list";
+    }
+})
+
+elPrev.addEventListener('click', () => {
+    elList.classList.remove("image__list"+counter);
+    counter--;
+})
+
+if (counter < 0) {
+    void (0)
+}
+
